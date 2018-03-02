12b4d0cea1d63406859d4d17f0ea01277b66b7d8
Matt Jones <gitcode@magisa.org>
2018-03-01 20:15:38 -0900

    Shifted the Apache config to start preparing to use mod_jk.

--- metacat.sh
+++ metacat.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+set -e
+
+if [ -z $1 ] ;
+  then
+    echo "Usage: $0 <version>"
+    exit
+fi
+
 #
-VER=2.8.4
-RELEASE="metacat-bin-${VER}.tar.gz"
+VERSION=$1
+RELEASE="metacat-bin-${VERSION}.tar.gz"
 DIST="https://knb.ecoinformatics.org/software/dist"
-export PGUSER=metacat
-export PGDB=metacat 
 
 # Grab the Metacat release
 if [ !  -f "dist/${RELEASE}" ]
